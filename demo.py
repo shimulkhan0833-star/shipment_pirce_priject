8afde9539f7e2de240d6af0a48edadae2d58642f
shimulkhan0833 <shimulkhan0833@gmail.com>
2026-06-20 02:30:43 +0600

    logger and exception added

---
+++ demo.py
@@ -0,0 +1,12 @@
+from shipment.logger import logging
+from shipment.exception import shippingException
+import sys
+
+try:
+    a = 1 / 0
+except Exception as e:
+    raise shippingException(e, sys) from e
+
+
+
+logging.info("This is a info message")
